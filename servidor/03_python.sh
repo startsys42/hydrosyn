0ac49f0833ca30c01500aff5ffd655f10acae26b
startsys <startsys@proton.me>
2025-05-06 15:15:53 +0000

    Update 03_python.sh

--- servidor/03_python.sh
+++ servidor/03_python.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+apt install -y python3 python3-pip python3-venv
+mkdir ~/fastapi_app
+cd ~/fastapi_app
+python3 -m venv venv
+source venv/bin/activate
+pip install fastapi uvicorn
 
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
