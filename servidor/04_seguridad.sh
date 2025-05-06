ccbc04bc19bb74d1041e2f1f69fbfaada54a5871
startsys <startsys@proton.me>
2025-05-06 21:31:33 +0000

    Update 04_seguridad.sh

--- servidor/04_seguridad.sh
+++ servidor/04_seguridad.sh
@@ -1,1 +1,7 @@
 
+BASHRC="/root/.bashrc"
+
+# Quitar el bloque entre los marcadores
+sed -i '/# === VARS_FROM_CONF_ENV ===/,/# === END_VARS_FROM_CONF_ENV ===/d' "$BASHRC"
+
+echo "Variables eliminadas de $BASHRC"
