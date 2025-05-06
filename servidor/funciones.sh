613ba1f0d8fba5910805fc3c0c27359072cbbd8c
startsys <startsys@proton.me>
2025-05-06 20:58:10 +0000

    Create funciones.sh

---
+++ servidor/funciones.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Definir la función en este script
+instalar_paquete() {
+  PAQUETE="$1"
+
+  # Instalar el paquete
+
+   apt-get install -y "$PAQUETE" > /dev/null 2>&1
+
+  # Verificar si la instalación fue exitosa
+  if [ $? -ne 0 ]; then
+
+    echo -e "\e[31mError: No se pudo instalar el paquete '$PAQUETE'.\e[0m"
+    exit 1 
+  fi
+}
