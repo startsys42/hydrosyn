9a271e61a2325f8bef62e2fa3b8b0f937de8acfa
startsys <startsys@proton.me>
2025-05-13 23:45:33 +0200

    Update 05_seguridad.sh

--- servidor/05_seguridad.sh
+++ servidor/05_seguridad.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-
-export MARKER_INI="# === VARS_FROM_CONF_ENV ==="
-export MARKER_END="# === END_VARS_FROM_CONF_ENV ==="
+COLOR_BG_IMPAR="46"
+COLOR_BG_PAR="43"
+ MARKER_INI="# === VARS_FROM_CONF_ENV ==="
+ MARKER_END="# === END_VARS_FROM_CONF_ENV ==="
 BASHRC="/root/.bashrc"
 
-# Quitar el bloque entre los marcadores
-sed -i '/# === VARS_FROM_CONF_ENV ===/,/# === END_VARS_FROM_CONF_ENV ===/d' "$BASHRC"
 
-echo "Variables eliminadas de $BASHRC"
+
+# Verifica si el bloque existe
+if grep -q "$MARKER_INI" "$BASHRC" && grep -q "$MARKER_END" "$BASHRC"; then
+    # Hacer una copia de respaldo opcional (recomendado)
+    cp "$BASHRC" "$BASHRC.bak"
+
+    # Eliminar el bloque
+    sed -i "/$MARKER_INI/,/$MARKER_END/d" "$BASHRC"
+
+    echo " Bloque de variables eliminado de $BASHRC"
+else
+    echo " No se encontró ningún bloque de variables en $BASHRC"
+fi
