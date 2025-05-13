23306f3c76c46f1a40a1dc8d05f25131a0b4e984
startsys <startsys@proton.me>
2025-05-13 23:52:46 +0200

    Update 04_nginx.sh

--- servidor/04_nginx.sh
+++ servidor/04_nginx.sh
@@ -2,3 +2,15 @@
 COLOR_BG_IMPAR="46"
 COLOR_BG_PAR="43"
 
+
+# Instalar Nginx
+instalar_paquete nginx 
+
+systemctl enable nginx
+systemctl start nginx
+
+systemctl status nginx --no-pager
+
+
+
+echo "🚀 Nginx instalado y en funcionamiento."
