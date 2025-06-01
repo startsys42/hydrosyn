983d7334d84f3ea5a4d4e82a41b01774639b7f33
startsys <startsys@proton.me>
2025-06-01 20:46:03 +0200

    Update 03_python.sh

--- servidor/03_python.sh
+++ servidor/03_python.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+#crear solo usuario especial para la app
 COLOR_BG_IMPAR="46"
 COLOR_BG_PAR="43"
+
 ins-paq python3
 if [ $? -ne 0 ]; then
  
@@ -22,8 +25,39 @@ fi
 
 mkdir /opt/hydrosyn
 cd  /opt/hydrosyn
-python3 -m venv venv
-source venv/bin/activate
-pip3 install fastapi uvicorn JINJA2
 
-uvicorn main:app --host 0.0.0.0 --port  $APP_PORT--reload
+python3 -m venv venv
+/opt/hydrosyn/venv/bin/pip install --upgrade pip
+/opt/hydrosyn/venv/bin/pip install fastapi uvicorn jinja2
+
+
+
+
+
+cat <<EOF > /etc/systemd/system/hydrosyn.service
+[Unit]
+Description=FastAPI app Hydrosyn
+After=network.target
+
+[Service]
+User=tu_usuario
+WorkingDirectory=/opt/hydrosyn
+ExecStart=/opt/hydrosyn/venv/bin/uvicorn main:app --host 0.0.0.0 --port $APP_PORT
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Cambiar propietario a root y permisos correctos
+chown root:root /etc/systemd/system/hydrosyn.service
+chmod 644 /etc/systemd/system/hydrosyn.service
+
+# Recargar systemd para detectar el nuevo servicio
+systemctl daemon-reload
+
+# Habilitar el servicio para que arranque al inicio
+systemctl enable hydrosyn.service
+
+# Iniciar el servicio ahora mismo
+systemctl start hydrosyn.service
