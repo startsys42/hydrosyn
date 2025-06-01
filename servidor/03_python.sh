26c4e81a505b073873fc2c0c2527f41f1bae8d3b
startsys <startsys@proton.me>
2025-06-01 16:26:03 +0200

    Update 03_python.sh

--- servidor/03_python.sh
+++ servidor/03_python.sh
@@ -2,7 +2,7 @@
 COLOR_BG_IMPAR="46"
 COLOR_BG_PAR="43"
 
-apt install -y python3 python3-pip python3-venv
+apt-get install -y python3 python3-pip python3-venv
 mkdir /opt/hydrosyn
 cd  /opt/hydrosyn
 python3 -m venv venv
