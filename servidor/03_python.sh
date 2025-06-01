e409bfd7e7ce5606f99c2a3f14433e8fc969f84d
startsys <startsys@proton.me>
2025-06-01 20:35:13 +0200

    Update 03_python.sh

--- servidor/03_python.sh
+++ servidor/03_python.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 COLOR_BG_IMPAR="46"
 COLOR_BG_PAR="43"
+ins-paq python3
+if [ $? -ne 0 ]; then
+ 
+    exit 1
+fi
 
-apt-get install -y python3 python3-pip python3-venv
+ins-paq python3-pip
+if [ $? -ne 0 ]; then
+ 
+    exit 1
+fi
+
+ins-paq python3-venv
+if [ $? -ne 0 ]; then
+ 
+    exit 1
+fi
+
+
 mkdir /opt/hydrosyn
 cd  /opt/hydrosyn
 python3 -m venv venv
 source venv/bin/activate
-pip install fastapi uvicorn
+pip3 install fastapi uvicorn JINJA2
 
 uvicorn main:app --host 0.0.0.0 --port  $APP_PORT--reload
