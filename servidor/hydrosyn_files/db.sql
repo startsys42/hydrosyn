b4269af3a08ca77085f354aade5606020a43d478
startsys <startsys@proton.me>
2025-06-05 19:31:21 +0200

    Update db.sql

--- servidor/hydrosyn_files/db.sql
+++ servidor/hydrosyn_files/db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS hydrosyn_db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_cs;
+CREATE DATABASE IF NOT EXISTS hydrosyn_db CHARACTER SET utf8mb4 COLLATE  utf8mb4_bin;
 USE hydrosyn_db;
 
 CREATE TABLE permissions (
