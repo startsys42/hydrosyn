28b435d941a1e344b0a64e714da1c180d390d353
startsys <startsys@proton.me>
2025-06-14 16:09:25 +0200

    Update ins-pip.sh

--- servidor/ins-pip.sh
+++ servidor/ins-pip.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-# Función para instalar paquetes de Python
-ins_pip() {
-    local PIP_BIN="$1"
-  shift
-  local PAQUETE="$1"
-  # Mostrar las variables
-  echo "Usando pip en: $PIP_BIN"
-  echo "Instalando paquete: $PAQUETE"
+# primer argumento: ruta del pip
+#argumentos restantes: paquetes python o array de paquetes
 
-  # Instalar el paquete (salida oculta)
+# Verificar que al menos se pasó el ejecutable pip
+if [[ $# -lt 2 ]]; then
+   echo -e "\e[30;41m Error:Faltan argumentos.\e[0m"
+  exit 1
+fi
+
+# Guardar ruta al ejecutable pip
+PIP_BIN="$1"
+shift
+
+# Detectar si el siguiente argumento es el nombre de una variable tipo array
+if [[ $# -eq 1 && "$(declare -p "$1" 2>/dev/null)" =~ "declare -a" ]]; then
+  eval "paquetes=(\"\${$1[@]}\")"
+else
+  paquetes=("$@")
+fi
+
+# Iterar e instalar cada paquete
+for PAQUETE in "${paquetes[@]}"; do
+
+
   "$PIP_BIN" install --upgrade "$PAQUETE" > /dev/null 2>&1
 
-
-  # Verificar si fue exitoso
-  if [ $? -ne 0 ]; then
+  if [[ $? -ne 0 ]]; then
     echo -e "\e[30;41m Error: No se pudo instalar el paquete Python '$PAQUETE'.\e[0m"
-    return 1
+    exit 1
   fi
-}
-
-
-ins_pip "$@"
+done
