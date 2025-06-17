f643f5321e97d07729adb2b66675566e9262b8ab
startsys <startsys@proton.me>
2025-06-17 14:17:22 +0200

    Update auth.py

--- servidor/hydrosyn_files/hydrosyn/app/web/auth.py
+++ servidor/hydrosyn_files/hydrosyn/app/web/auth.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
-router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
-@router.get("/login")
-async def mostrar_login(request: Request):
-    return templates.TemplateResponse("login_en.html", {"request": request})
+router = APIRouter()
 
-@router.get("/recover")
+@router.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@router.get("/recover-password", response_class=HTMLResponse)
 async def recover_password(request: Request):
-    return templates.TemplateResponse("recover.html", {"request": request})
+    return templates.TemplateResponse("recover_password.html", {"request": request})
