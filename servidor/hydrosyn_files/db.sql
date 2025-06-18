6136dc5cff6c7e04fb6efa43ab4d9dd126d2b8c6
startsys <startsys@proton.me>
2025-06-18 18:43:01 +0200

    Update db.sql

--- servidor/hydrosyn_files/db.sql
+++ servidor/hydrosyn_files/db.sql
@@ -22,7 +22,7 @@ CREATE TABLE permission_translations (
 
 CREATE TABLE roles (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL UNIQUE
  
 );
 
