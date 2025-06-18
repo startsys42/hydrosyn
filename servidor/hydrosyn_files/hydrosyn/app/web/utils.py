3b950d3c94c47cbedd961fc6713251c81dc26552
startsys <startsys@proton.me>
2025-06-18 21:34:56 +0200

    Create utils.py

---
+++ servidor/hydrosyn_files/hydrosyn/app/web/utils.py
@@ -0,0 +1,26 @@
+from fastapi import Request
+
+LANGS = {
+    "es": {
+        "title": "¡Bienvenid@!",
+        "login_title": "Iniciar sesión",
+        "username": "Usuario",
+        "password": "Contraseña",
+        "submit": "Enviar",
+        "back": "Volver",
+        "change_lang": "Cambiar idioma",
+        "change_theme": "Cambiar tema",
+        "forgot": "Recuperar contraseña",
+    },
+    "en": {
+        "title": "Welcome!",
+        "login_title": "Login",
+        "username": "Username",
+        "password": "Password",
+        "submit": "Submit",
+        "back": "Back",
+        "change_lang": "Change language",
+        "change_theme": "Change theme",
+        "forgot": "Recover password",
+    }
+}
