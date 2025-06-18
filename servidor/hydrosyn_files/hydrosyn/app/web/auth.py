8e15c79b1fcfb64197c370b8d1a12da199e6c385
startsys <startsys@proton.me>
2025-06-18 22:12:52 +0200

    Update auth.py

--- servidor/hydrosyn_files/hydrosyn/app/web/auth.py
+++ servidor/hydrosyn_files/hydrosyn/app/web/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-
+from utils import get_user_preferences 
 templates = Jinja2Templates(directory="app/templates")
 
 router = APIRouter()
