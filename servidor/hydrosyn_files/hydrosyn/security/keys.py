b1bf27ee335ba92bf81c54ca7c0ae75eff0bf386
startsys <startsys@proton.me>
2025-06-19 18:28:21 +0200

    Update keys.py

--- servidor/hydrosyn_files/hydrosyn/security/keys.py
+++ servidor/hydrosyn_files/hydrosyn/security/keys.py
@@ -1,7 +1,7 @@
 
 import secrets
 import time
-from typing import Tuple
+from typing import Tuple, Callable
 
 import math
 
@@ -10,15 +10,16 @@ def generar_clave_segura(longitud: int = 128) -> str:
     return secrets.token_urlsafe(n_bytes)[:longitud]  # recortar si se pasa
 
 class GestorClaves:
-    def __init__(self, tiempo_rotacion: int):
-        self.tiempo_rotacion = tiempo_rotacion  # en segundos
+    def __init__(self, obtener_tiempo_rotacion: Callable[[], int]):
+        self.obtener_tiempo_rotacion = obtener_tiempo_rotacion
         self.ultima_rotacion = time.time()
         self.key_session_new = generar_clave_segura()
         self.key_session_old = self.key_session_new
 
     def obtener_claves(self) -> Tuple[str, str]:
         ahora = time.time()
-        if ahora - self.ultima_rotacion >= self.tiempo_rotacion:
+        tiempo_rotacion_actual = self.obtener_tiempo_rotacion()
+        if ahora - self.ultima_rotacion >= tiempo_rotacion_actual:
             self.key_session_old = self.key_session_new
             self.key_session_new = generar_clave_segura()
             self.ultima_rotacion = ahora
