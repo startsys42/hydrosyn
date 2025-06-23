77189f7c0938d549490046a709a329890cb4e939
startsys <startsys@proton.me>
2025-06-23 07:21:51 +0200

    Update 1.sh

--- servidor/pterodactyl/1.sh
+++ servidor/pterodactyl/1.sh
@@ -12,7 +12,7 @@ echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://pack
 apt update
 
 # Install Dependencies
-apt -y install php8.3 php8.3-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+apt -y install php8.2 php8.2-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
 
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
@@ -22,3 +22,100 @@ cd /var/www/pterodactyl
 curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
 tar -xzvf panel.tar.gz
 chmod -R 755 storage/* bootstrap/cache/
+MYSQL_ROOT_PASSWORD="rootPassword"
+PTERODACTYL_PASSWORD="yourPassword"
+
+# Ejecutar comandos SQL con mysql CLI, usando localhost
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -h localhost <<EOF
+CREATE USER 'pterodactyl'@'localhost' IDENTIFIED BY '$PTERODACTYL_PASSWORD';
+CREATE DATABASE panel;
+GRANT ALL PRIVILEGES ON panel.* TO 'pterodactyl'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
+
+
+
+cp .env.example .env
+COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --optimize-autoloader
+
+# Only run the command below if you are installing this Panel for
+# the first time and do not have any Pterodactyl Panel data in the database.
+php artisan key:generate --force
+
+php artisan p:environment:setup
+php artisan p:environment:database
+
+# To use PHP's internal mail sending (not recommended), select "mail". To use a
+# custom SMTP server, select "smtp".
+php artisan p:environment:mail
+
+php artisan migrate --seed --force
+
+php artisan p:user:make
+
+chown -R nginx:nginx /var/www/pterodactyl/*
+
+* * * * * php /var/www/pterodactyl/artisan schedule:run >> /dev/null 2>&1
+
+
+SERVICE_FILE="/etc/systemd/system/pteroq.service"
+
+sudo tee "$SERVICE_FILE" > /dev/null <<EOF
+[Unit]
+Description=Pterodactyl Queue Worker
+After=redis-server.service
+
+[Service]
+# On some systems the user and group might be different.
+# Some systems use \`apache\` or \`nginx\` as the user and group.
+User=www-data
+Group=www-data
+Restart=always
+ExecStart=/usr/bin/php /var/www/pterodactyl/artisan queue:work --queue=high,standard,low --sleep=3 --tries=3
+StartLimitInterval=180
+StartLimitBurst=30
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl enable --now redis-server
+sudo systemctl enable --now pteroq.service
+
+curl -sSL https://get.docker.com/ | CHANNEL=stable bash
+
+sudo systemctl enable --now docker
+#GRUB_CMDLINE_LINUX_DEFAULT="swapaccount=1"
+
+sudo mkdir -p /etc/pterodactyl
+curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+sudo chmod u+x /usr/local/bin/wings
+
+sudo wings --debug
+
+SERVICE_FILE="/etc/systemd/system/wings.service"
+
+sudo tee "$SERVICE_FILE" > /dev/null <<EOF
+[Unit]
+Description=Pterodactyl Wings Daemon
+After=docker.service
+Requires=docker.service
+PartOf=docker.service
+
+[Service]
+User=root
+WorkingDirectory=/etc/pterodactyl
+LimitNOFILE=4096
+PIDFile=/var/run/wings/daemon.pid
+ExecStart=/usr/local/bin/wings
+Restart=on-failure
+StartLimitInterval=180
+StartLimitBurst=30
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl enable --now wings
