220ae484dd82f83b72843a762a73eb6f2706b02f
startsys <startsys@proton.me>
2025-06-23 07:41:00 +0200

    Update 1.sh

--- servidor/pterodactyl/1.sh
+++ servidor/pterodactyl/1.sh
@@ -50,10 +50,64 @@ php artisan p:environment:database
 php artisan p:environment:mail
 
 php artisan migrate --seed --force
-
+sudo useradd -r -s /sbin/nologin nginx
 php artisan p:user:make
-
+rm /etc/nginx/sites-enabled/default
 chown -R nginx:nginx /var/www/pterodactyl/*
+# You do not need to symlink this file if you are using RHEL, Rocky Linux, or AlmaLinux.
+sudo ln -s /etc/nginx/sites-available/pterodactyl.conf /etc/nginx/sites-enabled/pterodactyl.conf
+
+# You need to restart nginx regardless of OS.
+sudo systemctl restart nginx
+
+
+CONFIG_PATH="/etc/nginx/sites-available/pterodactyl"
+
+sudo tee "$CONFIG_PATH" > /dev/null <<EOF
+server {
+    listen 80;
+    server_name localhost;
+
+    root /var/www/pterodactyl/public;
+    index index.html index.htm index.php;
+    charset utf-8;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
+    access_log off;
+    error_log  /var/log/nginx/pterodactyl.app-error.log error;
+
+    client_max_body_size 100m;
+    client_body_timeout 120s;
+
+    sendfile off;
+
+    location ~ \\.php\$ {
+        fastcgi_split_path_info ^(.+\\.php)(/.+)\$;
+        fastcgi_pass unix:/run/php/php8.3-fpm.sock;
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param PHP_VALUE "upload_max_filesize = 100M \\n post_max_size=100M";
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_param HTTP_PROXY "";
+        fastcgi_intercept_errors off;
+        fastcgi_buffer_size 16k;
+        fastcgi_buffers 4 16k;
+        fastcgi_connect_timeout 300;
+        fastcgi_send_timeout 300;
+        fastcgi_read_timeout 300;
+    }
+
+    location ~ /\\.ht {
+        deny all;
+    }
+}
+EOF
 
 * * * * * php /var/www/pterodactyl/artisan schedule:run >> /dev/null 2>&1
 
