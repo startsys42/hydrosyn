92099c16546187845bc4d293d417dfdeb0625100
startsys <startsys@proton.me>
2025-06-24 15:52:56 +0200

    Update main.py

--- servidor/hydrosyn_files/hydrosyn/main.py
+++ servidor/hydrosyn_files/hydrosyn/main.py
@@ -12,6 +12,7 @@ import os
 import sys
 from db.conexion import inicializar_engine
 from datetime import datetime
+from datetime import time
 
 import asyncio
 from datetime import datetime, timedelta
