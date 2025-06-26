caa44bb98e80bbdc92976a6d3754e98eb0601ef7
startsys <startsys@proton.me>
2025-06-26 18:13:12 +0200

    Update startup.py

--- servidor/hydrosyn_files/hydrosyn/events/startup.py
+++ servidor/hydrosyn_files/hydrosyn/events/startup.py
@@ -1,1 +1,8 @@
+import asyncio
+from tasks.sessions import periodic_cleanup_job
+from logger import logger  # Importa tu logger personalizado
 
+async def on_startup():
+    logger.info("La aplicación está arrancando...")
+    # Lanzar el job periódico en segundo plano
+    asyncio.create_task(periodic_cleanup_job())
