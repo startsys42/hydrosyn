3793e70953185b43adf9cba39e174915b61fde0d
startsys <startsys@proton.me>
2025-06-28 14:22:02 +0200

    Update auth.py

--- servidor/hydrosyn_files/hydrosyn/app/web/auth.py
+++ servidor/hydrosyn_files/hydrosyn/app/web/auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from app.web.utils import get_user_preferences
-
+from fastapi.responses import PlainTextResponse
 templates = Jinja2Templates(directory="app/templates")
 
 router = APIRouter()
@@ -12,7 +12,7 @@ async def login(request: Request):
     try:
         prefs = get_user_preferences(request)
     except ValueError as e:
-        from fastapi.responses import PlainTextResponse
+        
         return PlainTextResponse(str(e), status_code=400)
 
     return templates.TemplateResponse("login.html", {
