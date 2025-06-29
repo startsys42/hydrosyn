8a4f9991185e819e3cc3f5462fa085251910303a
startsys <startsys@proton.me>
2025-06-29 18:00:47 +0200

    Update db1_config_tables.sql

--- servidor/hydrosyn_files/db1_config_tables.sql
+++ servidor/hydrosyn_files/db1_config_tables.sql
@@ -35,14 +35,16 @@ INSERT INTO config (value, min_value, max_value) VALUES
 (1, 1, 90),      -- Refresh token duration (in days)
 (2, 0, 23),      -- Daily cleanup hour sessions and tokens (0 to 23)
 (24, 1, 24),    -- Time limit to verify email or change password (in hours)
-(365, 1, 1825),  -- Days to retain unverified users before deletion
+(365, 90, 1825),  -- Days to retain unverified users before deletion
 (2, 1, 10);      -- Time to force username change after policy update (in days)
+(365, 90, 1825),  -- Days to retain username policy history before deletion
 (2, 1, 10);      -- Time to force password change after policy update (in days)
-
-(365, 1, 1825),  -- Days to retain users activation history before deletion
-(365, 1, 1825),  -- Days to retain users email changes before deletion
-
-
+(365, 90, 1825),  -- Days to retain password policy history before deletion
+(365, 90, 1825),  -- Days to retain users activation history before deletion
+(365, 90, 1825),  -- Days to retain users email changes before deletion
+    
+(365, 90, 1825),  -- Days to retain login attempts before deletion
+login_attempts
 -- Para intentos de sesión
 INSERT INTO config_translations (config_id, lang_code, name, description)
 VALUES 
