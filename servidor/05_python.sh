a824ba0b826841c2fe12b6e5befb8f2459db310c
startsys <startsys@proton.me>
2025-06-30 19:13:46 +0200

    Update 05_python.sh

--- servidor/05_python.sh
+++ servidor/05_python.sh
@@ -165,8 +165,8 @@ chmod 644 /etc/systemd/system/hydrosyn.service
 
 cat <<EOF > /opt/hydrosyn/.env
 LOG_DIR=logs
-DB_USER=hydro_user
-DB_HOST=127.0.0.1
+DB_USER=$DB_USER_HYDRO
+DB_HOST=$DB_IP
 DB_NAME=hydrosyn_db
 EOF
 
