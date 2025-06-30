dd4a2aaa46aa094cb7309bd238567e6c0bbded67
startsys <startsys@proton.me>
2025-06-30 19:56:06 +0200

    Update main.py

--- servidor/hydrosyn_files/hydrosyn/main.py
+++ servidor/hydrosyn_files/hydrosyn/main.py
@@ -60,7 +60,7 @@ cookie_key_manager = CookieKeyManager(
     ttl=600  # 10 minutes 
 )
 
-jwt_key_manager = JWTKeyManager(get_rotation_time=get_jwt_rotation_time_from_db, ttl=600)
+
 
 app = FastAPI()
 @app.on_event("startup")
