597871fa4d1e4806a94263ce4988db983770cba7
startsys <startsys@proton.me>
2025-06-30 17:47:45 +0200

    Update db_engine.py

--- servidor/hydrosyn_files/hydrosyn/db/db_engine.py
+++ servidor/hydrosyn_files/hydrosyn/db/db_engine.py
@@ -1,28 +1,29 @@
 from sqlalchemy import create_engine
-from security.secrets import get_most_recent_password, cargar_datos_maestros
 from logger import logger
 
-_engine = None
+class DBEngine:
+    _engine = None
 
-def initialize_engine(user, password, host, port, db_name):
-    global _engine
-    logger.info(f"Connecting to DB with: user={user}, password={'***' if password else 'EMPTY'}, host={host}, port={port}, db={db_name}")
+    @classmethod
+    def initialize_engine(cls, user, password, host, port, db_name):
+      #  logger.info(f"Connecting to DB with: user={user}, password={'***' if password else 'EMPTY'}, host={host}, port={port}, db={db_name}")
 
-    if not user or not password or not host or not port or not db_name:
-        raise ValueError("All connection parameters must have a valid value.")
+        if not user or not password or not host or not port or not db_name:
+            raise ValueError("All connection parameters must have a valid value.")
 
-    if not _engine:
-        try:
-            _engine = create_engine(
-                f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}",
-                pool_pre_ping=True
-            )
-            logger.info("Database engine initialized successfully.")
-        except Exception as e:
-            logger.error(f"Error initializing DB engine: {e}")
-            raise
+        if cls._engine is None:
+            try:
+                cls._engine = create_engine(
+                    f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}",
+                    pool_pre_ping=True
+                )
+                logger.info("Database engine initialized successfully.")
+            except Exception as e:
+                logger.error(f"Error initializing DB engine: {e}")
+                raise
 
-def get_engine():
-    if not _engine:
-        raise RuntimeError("Engine not initialized. Call 'initialize_engine' first.")
-    return _engine
+    @classmethod
+    def get_engine(cls):
+        if cls._engine is None:
+            raise RuntimeError("Engine not initialized. Call 'initialize_engine' first.")
+        return cls._engine
