916866c8bb3e3db38e3f6ac27dd3a65c04b3c09d
startsys <startsys@proton.me>
2025-06-30 18:00:46 +0200

    Update main.py

--- servidor/hydrosyn_files/hydrosyn/main.py
+++ servidor/hydrosyn_files/hydrosyn/main.py
@@ -22,7 +22,7 @@ from db.config import obtener_hora_limpieza_desde_bd
 from db.conexion import get_engine
 from sqlalchemy import text
 from logger import logger
-
+from db_engine import DBEngine 
 
 
 
