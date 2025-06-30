14dfe5810b2fc9ce59ec3c720c6e56adbefe4e2b
startsys <startsys@proton.me>
2025-06-30 15:34:46 +0200

    Update engine.py

--- servidor/hydrosyn_files/hydrosyn/db/engine.py
+++ servidor/hydrosyn_files/hydrosyn/db/engine.py
@@ -4,26 +4,25 @@ from logger import logger
 
 _engine = None
 
-def initialize_engine(usuario, password, host, puerto, nombre_db):
-   
+def initialize_engine(user, password, host, port, db_name):
     global _engine
-    logger.info(f"Conectando a BD con: usuario={usuario}, password={'***' if password else 'VACÍO'}, host={host}, puerto={puerto}, bd={nombre_db}")
+    logger.info(f"Connecting to DB with: user={user}, password={'***' if password else 'EMPTY'}, host={host}, port={port}, db={db_name}")
 
-    if not usuario or not password or not host or not puerto or not nombre_db:
-        raise ValueError("Todos los parámetros de conexión deben tener valor válido.")
+    if not user or not password or not host or not port or not db_name:
+        raise ValueError("All connection parameters must have a valid value.")
 
     if not _engine:
         try:
             _engine = create_engine(
-                f"mysql+pymysql://{usuario}:{password}@{host}:{puerto}/{nombre_db}",
+                f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}",
                 pool_pre_ping=True
             )
-            logger.info("Motor de base de datos inicializado correctamente.")
+            logger.info("Database engine initialized successfully.")
         except Exception as e:
-            logger.error(f"Error al inicializar motor DB: {e}")
+            logger.error(f"Error initializing DB engine: {e}")
             raise
 
 def get_engine():
     if not _engine:
-        raise RuntimeError("Engine no inicializado. Llamar primero a 'inicializar_engine'.")
+        raise RuntimeError("Engine not initialized. Call 'initialize_engine' first.")
     return _engine
