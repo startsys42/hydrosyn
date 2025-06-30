ae7d3290da4b2e2a5352907b7e2c03f5db0382a5
startsys <startsys@proton.me>
2025-06-30 22:15:07 +0200

    Update tokens.py

--- servidor/hydrosyn_files/hydrosyn/app/api/tokens.py
+++ servidor/hydrosyn_files/hydrosyn/app/api/tokens.py
@@ -1,3 +1,5 @@
+from security.keys import JWTKeyManager
+from db.sb_config import get_jwt_rotation_time_from_db
 
 
 jwt_key_manager = JWTKeyManager(get_rotation_time=get_jwt_rotation_time_from_db, ttl=600)
