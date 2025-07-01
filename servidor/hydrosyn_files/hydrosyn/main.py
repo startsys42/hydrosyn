7f3f8b4c0bf42f9ad733e8fd13bbeca201f085b6
startsys <startsys@proton.me>
2025-07-01 16:10:54 +0200

    Update main.py

--- servidor/hydrosyn_files/hydrosyn/main.py
+++ servidor/hydrosyn_files/hydrosyn/main.py
@@ -6,7 +6,7 @@ import sys
 from logger import logger
 from security.secrets import get_most_recent_password, load_master_data
 from security.keys import CookieKeyManager
-from db.db_config import get_cookie_rotation_time_from_db
+from db.db_config import get_cookie_rotation_time_from_db, get_old_cookie_limit_hour_from_db
 from db.db_engine import DBEngine
 from security.middleware import DualSessionMiddleware
 from dotenv import load_dotenv
