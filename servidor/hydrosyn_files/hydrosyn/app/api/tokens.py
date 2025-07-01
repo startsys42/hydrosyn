7aba0dd91bd7998b95b18ea4cb67a7a173dafb58
startsys <startsys@proton.me>
2025-07-01 17:11:16 +0200

    Update tokens.py

--- servidor/hydrosyn_files/hydrosyn/app/api/tokens.py
+++ servidor/hydrosyn_files/hydrosyn/app/api/tokens.py
@@ -2,4 +2,4 @@ from security.keys import JWTKeyManager
 from db.sb_config import get_jwt_rotation_time_from_db
 
 
-jwt_key_manager = JWTKeyManager(get_rotation_time=get_jwt_rotation_time_from_db, ttl=600)
+jwt_key_manager = JWTKeyManager(get_rotation_time=get_jwt_rotation_time_from_db, get_grace_period = get_old_cookie_token_limit_hour_from_db,ttl=600)
