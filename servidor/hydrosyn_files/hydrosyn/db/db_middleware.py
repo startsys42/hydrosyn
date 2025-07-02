3ef8b98f78edbb3ad22ec5969cc5270a491348d5
startsys <startsys@proton.me>
2025-07-02 14:46:14 +0200

    Update db_middleware.py

--- servidor/hydrosyn_files/hydrosyn/db/db_middleware.py
+++ servidor/hydrosyn_files/hydrosyn/db/db_middleware.py
@@ -3,6 +3,27 @@ from datetime import datetime, timedelta
 from db.db_engine import DBEngine
 from logger import logger
 
+def get_cookie_expired_time_from_db() -> int:
+    try:
+        with DBEngine.get_engine().connect() as conn:
+            result = conn.execute(
+                text("SELECT  max_value FROM config WHERE id = 4")
+            ).fetchone()
+
+            if result:
+                 max_val = result
+                if max_val > 0:
+                    return int(max_val)
+                else:
+                    logger.warning(f"Cookie expired time {max_val} error. Using default.")
+    except Exception as e:
+        logger.error(f"Error fetching cookie expired time : {e}")
+
+ 
+    logger.info(f"Using default cookie  expired time: 30 days")
+    return 30
+
+
 def get_session_from_db(session_id: str, extend_validity: bool = True) -> dict:
     """
     Obtiene los datos de sesión desde la base de datos con opción de margen de validez
