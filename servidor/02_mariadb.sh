eb59a90fe1d73530055e95396e882f53ad7d5ffd
startsys <startsys@proton.me>
2025-07-02 20:21:33 +0200

    Update 02_mariadb.sh

--- servidor/02_mariadb.sh
+++ servidor/02_mariadb.sh
@@ -153,11 +153,11 @@ systemctl status mariadb
 
 echo -e "\e[32mMySQL asegurado correctamente.\e[0m"
 
-$MYSQL -u root -p"$MYSQL_ROOT_PASSWORD"< hydrosyn_files/db1_config_tables.sql
+$MYSQL -u root -p"$MYSQL_ROOT_PASSWORD"< files/db/hydrosyn/db1_config_tables.sql
 
-$MYSQL -u root -p"$MYSQL_ROOT_PASSWORD"< hydrosyn_files/db2_users_tables.sql
-$MYSQL -u root -p"$MYSQL_ROOT_PASSWORD"< hydrosyn_files/db3_systems_tables.sql
-$MYSQL -u root -p"$MYSQL_ROOT_PASSWORD"< hydrosyn_files/db4_permissions_tables.sql
+$MYSQL -u root -p"$MYSQL_ROOT_PASSWORD"< files/db/hydrosyn/db2_users_tables.sql
+$MYSQL -u root -p"$MYSQL_ROOT_PASSWORD"< files/db/hydrosyn/db3_systems_tables.sql
+$MYSQL -u root -p"$MYSQL_ROOT_PASSWORD"< files/db/hydrosyn/db4_permissions_tables.sql
 # Generar hash bcrypt con Python
 HASH_PASS=$(python3 -c "
 import bcrypt
