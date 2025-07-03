01a4f12a1d7a1323d413046e18ce46a0baada5d7
startsys <startsys@proton.me>
2025-07-03 17:38:06 +0200

    Update db_auth.py

--- servidor/files/hydrosyn/db/db_auth.py
+++ servidor/files/hydrosyn/db/db_auth.py
@@ -5,6 +5,24 @@ from db.conexion import get_engine
 from logger import logger  # Asegúrate de tenerlo configurado
 from db.config import obtener_tiempo_rotacion_desde_bd
 
+def delete_session_from_db(session_id: str) -> bool:
+    sql = text("""
+        DELETE FROM sessions WHERE session_id = :session_id
+    """)
+    try:
+        with get_engine().connect() as conn:
+            result = conn.execute(sql, {"session_id": session_id})
+            conn.commit()  # Commit the transaction
+            if result.rowcount > 0:
+                logger.info(f"Session deleted successfully. Session ID: {session_id}")
+                return True
+            else:
+                logger.warning(f"No session found with Session ID: {session_id}")
+                return False
+    except Exception as e:
+        logger.error(f"Error deleting session with Session ID {session_id}: {e}")
+        return False
+
 def email_existe(email: str) -> bool:
     sql = text("""
         SELECT 1 FROM users WHERE email = :email LIMIT 1
