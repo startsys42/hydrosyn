0212e88f7efd1c78449ed6e4e72ae9cb61d3a1d1
startsys <startsys@proton.me>
2025-07-06 18:36:10 +0200

    Update middleware_web.py

--- servidor/files/hydrosyn/security/middleware_web.py
+++ servidor/files/hydrosyn/security/middleware_web.py
@@ -57,14 +57,14 @@ class AdvancedSessionMiddleware(BaseHTTPMiddleware):
                     is_logged_in = True
                     request.state.user_id = user_id
                     if is_logged_in:
-                        if request.url.path in [ "/web/auth/recover-password", "/web/auth/login-two", "/web/auth/recover-password-two","/","/web/auth/login" ]:
+                        if request.url.path in [ "/web/auth/login","/web/auth/recover-password", "/web/auth/login-two", "/web/auth/recover-password-two","/",]:
                             return RedirectResponse(url="/dashboard")  # o la ruta del home de usuario
             else:
-                if request.url.path not in [ "/web/auth/recover-password", "/web/auth/login-two", "/web/auth/recover-password-two","/","/device-info","/web/auth/login" ]:
+                if request.url.path not in [ "/web/device/device-info","/web/auth/login" ,"/web/auth/recover-password", "/web/auth/login-two", "/web/auth/recover-password-two","/"]:
                     return RedirectResponse(url="/", status_code=303)
         else:
         # No hay sesión, permitir solo acceso a rutas públicas
-            if request.url.path not in [ "/web/auth/recover-password", "/web/auth/login-two", "/web/auth/recover-password-two","/","/device-info","/web/auth/login" ]:
+            if request.url.path not in ["/web/device/device-info","/web/auth/login" , "/web/auth/recover-password", "/web/auth/login-two", "/web/auth/recover-password-two","/"]:
                 return RedirectResponse(url="/", status_code=303)
         
    
