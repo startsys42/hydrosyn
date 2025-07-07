a0f3b8a02e73a68d72492278c16d416b1baf245c
startsys <startsys@proton.me>
2025-07-07 18:17:10 +0200

    Update profile.py

--- servidor/files/hydrosyn/app/web/profile.py
+++ servidor/files/hydrosyn/app/web/profile.py
@@ -1,3 +1,3 @@
 from fastapi import APIRouter, Request
 router = APIRouter()
-# cambair nombre, cambair idioma, cambiar contarseña, activar twofa, desativar twofa, actiavr  , reajustar base de datso, cambair  la contraseña al suaurio,
+# cambair nombre, cambair idioma, cambiar contarseña, activar twofa, desativar twofa, actiavr  , cambair correo
