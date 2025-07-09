9de52922dd66cd1e7bc61ae6b63a6711e28f84da
startsys <startsys@proton.me>
2025-07-09 11:25:28 +0200

    Update startup.py

--- servidor/files/hydrosyn/events/startup.py
+++ servidor/files/hydrosyn/events/startup.py
@@ -1,26 +1,28 @@
 import asyncio
-
+from datetime import datetime, time, timedelta
 from logger import logger  # Import your custom logger
 from events.tasks import *
 
-def get_next_cleanup_time():
+async def get_next_cleanup_time():
     now = datetime.utcnow()
-    cleanup_hour = get_old_cookie_token_limit_hour_from_db()
+    cleanup_hour = await get_old_cookie_token_limit_hour_from_db()
     scheduled_time_today = datetime.combine(now.date(), time(cleanup_hour, 0))
 
     if now >= scheduled_time_today:
-        # Si ya pasó la hora de limpieza hoy, planificamos para mañana
         next_run = scheduled_time_today + timedelta(days=1)
     else:
         next_run = scheduled_time_today
 
     return next_run
 
+
+ 
+
 async def periodic_event_dynamic_schedule(name: str, get_next_run_time, task_coro, check_interval: int = 1800):
  while True:
         now = datetime.utcnow()
         try:
-            next_run = get_next_run_time()
+            next_run = await get_next_run_time()
         except Exception as e:
             logger.exception(f"Error getting next run time for '{name}': {e}")
             next_run = now + timedelta(seconds=check_interval)
@@ -38,6 +40,8 @@ async def periodic_event_dynamic_schedule(name: str, get_next_run_time, task_cor
 
 
 async def on_startup():
+    await cookie_key_manager.initialize()
+    await jwt_key_manager.initialize()
     asyncio.create_task(
         periodic_event_dynamic_schedule(
             name="General Clean",
