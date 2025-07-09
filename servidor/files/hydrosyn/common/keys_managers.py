0a185ef0ae11bb255780f7a7c1fdc76990646ce1
startsys <startsys@proton.me>
2025-07-09 14:09:17 +0200

    Create keys_managers.py

---
+++ servidor/files/hydrosyn/common/keys_managers.py
@@ -0,0 +1,14 @@
+from security.keys import CookieKeyManager, JWTKeyManager
+from db.db_config import get_cookie_rotation_time_from_db, get_old_cookie_token_limit_hour_from_db, get_jwt_rotation_time_from_db
+
+cookie_key_manager = CookieKeyManager(
+    get_rotation_time=get_cookie_rotation_time_from_db,
+    get_grace_period=get_old_cookie_token_limit_hour_from_db,
+    ttl=600
+)
+
+jwt_key_manager = JWTKeyManager(
+    get_rotation_time=get_jwt_rotation_time_from_db,
+    get_grace_period=get_old_cookie_token_limit_hour_from_db,
+    ttl=600
+)
