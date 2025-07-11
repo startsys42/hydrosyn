d8d1ba3fb89869a113778550d2975d7eb64784e1
startsys <startsys@proton.me>
2025-07-11 03:16:56 +0200

    Update users.py

--- servidor/files/hydrosyn/app/web/users.py
+++ servidor/files/hydrosyn/app/web/users.py
@@ -1,3 +1,5 @@
 
 from fastapi import APIRouter, Request
 router = APIRouter()
+
+# crear susuario , borarr usuario, desactivar usuario, cambairle contraseña o correo o 2fa # protlongar autentificar  o or ecuperar contraseña
