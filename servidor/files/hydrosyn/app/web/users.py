9819b729ca4e8238b02d9b91b9f27b34fac7c0fd
startsys <startsys@proton.me>
2025-07-12 15:28:11 +0200

    Update users.py

--- servidor/files/hydrosyn/app/web/users.py
+++ servidor/files/hydrosyn/app/web/users.py
@@ -4,4 +4,20 @@ router = APIRouter()
 
 # crear susuario , borarr usuario, desactivar usuario, cambairle contraseña o correo o 2fa
 # protlongar autentificar  o or ecuperar contraseña extender tiempo recuepracion oa activacion
-#listar permisos roles istemas
+#listar permisos roles istemas, listar usuarios
+
+@app.get("/create-user")
+async def change_name_get():
+    return {"action": "get name change form"}
+
+@app.post("/create-user")
+async def change_name_post():
+    return {"action": "process name change"}
+
+@app.get("/delete-user")
+async def change_name_get():
+    return {"action": "get name change form"}
+
+@app.post("/delete-user")
+async def change_name_post():
+    return {"action": "process name change"}
