bdd392a32af79729df90e972079e455e62d1747b
startsys <startsys@proton.me>
2025-07-12 15:24:35 +0200

    Update permissions.py

--- servidor/files/hydrosyn/app/web/permissions.py
+++ servidor/files/hydrosyn/app/web/permissions.py
@@ -1,2 +1,8 @@
 from fastapi import APIRouter, Request
 router = APIRouter()
+
+# consultar permisos  usuarios o sistema ¿ que usuarios tienen permiso o rl x en o en que sistema?
+#
+# cerar roles 
+# consultar rles ¿ roles existen, que roles con x permisos?
+# modificar borra roels
