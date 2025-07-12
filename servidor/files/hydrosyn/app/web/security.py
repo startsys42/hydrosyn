28e1f868e43a64cb1e3a4a3ddebc7df6801096d7
startsys <startsys@proton.me>
2025-07-12 14:00:28 +0200

    Update security.py

--- servidor/files/hydrosyn/app/web/security.py
+++ servidor/files/hydrosyn/app/web/security.py
@@ -2,7 +2,7 @@
 # cambair contraseña suaurio db
 # reajsutar db
 # cambair clave base de datos, revocar cookies
-extender tiempo suaurios login o recueparrc ontarseña
+#extender tiempo suaurios login o recueparrc ontarseña ver logs
 
 
 @router.get("/change-db-user-password")
