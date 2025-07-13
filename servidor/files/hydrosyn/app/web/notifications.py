cb216672ba8b8eafa24b33799afa40c1485935e2
startsys <startsys@proton.me>
2025-07-13 05:17:39 +0200

    Update notifications.py

--- servidor/files/hydrosyn/app/web/notifications.py
+++ servidor/files/hydrosyn/app/web/notifications.py
@@ -1,3 +1,3 @@
 from fastapi import APIRouter, Request
 router = APIRouter()
-## ver notificaciones, listar notificaciones , filtrar  
+## ver notificaciones, listar notificaciones , filtrar  , configurar no borrar notificacione sno leidas, marcar como elidas
