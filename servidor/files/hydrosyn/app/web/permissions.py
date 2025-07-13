db3e1e6c38aaa141fa50d2581451b0bd4ed8307b
startsys <startsys@proton.me>
2025-07-13 05:15:23 +0200

    Delete servidor/files/hydrosyn/app/web/permissions.py

--- servidor/files/hydrosyn/app/web/permissions.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Request
-router = APIRouter()
-
-# consultar permisos  usuarios o sistema ¿ que usuarios tienen permiso o rl x en o en que sistema?
-#
-# cerar roles 
-# consultar rles ¿ roles existen, que roles con x permisos?
-# modificar borra roels
