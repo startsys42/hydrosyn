fee4b131e40ffa15988c110104aca42908fbc317
startsys <startsys@proton.me>
2025-07-13 04:17:41 +0200

    Update db3_notifications_tables.sql

--- servidor/files/db/hydrosyn/db3_notifications_tables.sql
+++ servidor/files/db/hydrosyn/db3_notifications_tables.sql
@@ -1,10 +1,36 @@
 CREATE DATABASE IF NOT EXISTS hydrosyn_db CHARACTER SET utf8mb4 COLLATE  utf8mb4_bin;
 USE hydrosyn_db;
 
+-- ENCENDIDO SEREVR ROBOD E COOKIES usuario lista engra intenta loguearse od esactivado
 CREATE TABLE notifications (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-
+     should_send_email BOOLEAN NOT NULL DEFAULT FALSE,  -- Control por notificación
+  
 );
+CREATE TABLE notification_email_history (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    changed_by INT UNSIGNED NOT NULL,
+    changed_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY (changed_by) REFERENCES users(id) 
+        ON DELETE RESTRICT 
+        ON UPDATE CASCADE
+) ENGINE=InnoDB;
+CREATE TABLE notification_history (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    notification_id INT UNSIGNED NOT NULL,
+    changed_by INT UNSIGNED NOT NULL,
+    changed_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    should_send_email_old_value BOOLEAN NOT NULL,
+    
+    FOREIGN KEY (notification_id) REFERENCES notifications(id) 
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT,
+    FOREIGN KEY (changed_by) REFERENCES users(id) 
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+) ENGINE=InnoDB;
 
 
 CREATE TABLE notification_translations (
@@ -12,7 +38,7 @@ CREATE TABLE notification_translations (
     notification_id INT UNSIGNED NOT NULL,
     lang_code ENUM('es', 'en') NOT NULL,
     description VARCHAR(255) NOT NULL UNIQUE,
-
+template_text VARCHAR(255) NOT NULL,
     FOREIGN KEY (notification_id) REFERENCES notifications(id)
         ON DELETE RESTRICT
         ON UPDATE RESTRICT,
@@ -27,12 +53,12 @@ CREATE TABLE user_notifications (
     notification_id INT UNSIGNED NOT NULL,
     is_read BOOLEAN DEFAULT FALSE,
     read_at DATETIME DEFAULT NULL,
-
+formatted_message VARCHAR(255) NOT NULL
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE RESTRICT
         ON UPDATE CASCADE,
 
     FOREIGN KEY (notification_id) REFERENCES notifications(id)
         ON DELETE RESTRICT
-        ON UPDATE CASCADE
+        ON UPDATE RESTRICT
 );
