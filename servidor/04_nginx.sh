1e63392b6068e5f33e41e01e4d8c4956a8c86088
startsys42 <startsys@proton.me>
2025-07-25 20:54:30 +0200

    43545

--- servidor/04_nginx.sh
+++ servidor/04_nginx.sh
@@ -17,3 +17,45 @@ systemctl status nginx --no-pager
 
 
 
+cat > /etc/nginx/sites-available/hydrosyn << "EOF"
+server {
+    listen 80;
+    server_name hydrosyn.com;  # Cambia por tu dominio/IP
+
+    root /var/www/hydrosyn/build;
+    index index.html;
+
+    location / {
+        try_files $uri $uri/ /index.html;
+    }
+
+   
+}
+EOF
+
+
+ chown root:root /etc/nginx/sites-available/hydrosyn
+ chmod 644 /etc/nginx/sites-available/hydrosyn
+
+# Habilita el sitio
+ ln -s /etc/nginx/sites-available/hydrosyn /etc/nginx/sites-enabled/
+
+
+mkdir -p /var/www/hydrosyn
+
+# 2. Mover el build de React a la nueva ubicación
+# (Desde el directorio donde está tu build actual)
+ mv hydrosyn-react/build/ /var/www/hydrosyn/
+
+# 3. Asignar propietario adecuado (www-data para Nginx)
+ chown -R www-data:www-data /var/www/hydrosyn
+
+# 4. Dar permisos seguros:
+ chmod -R 755 /var/www/hydrosyn
+
+# 5. Verificar la estructura resultante:
+tree /var/www/hydrosyn
+
+
+# Verifica y reinicia Nginx
+ nginx -t &&  systemctl restart nginx
