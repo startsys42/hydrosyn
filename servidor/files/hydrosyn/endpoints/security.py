17039882095bd3d2f859f60ae4858839d7402610
startsys42 <startsys@proton.me>
2025-07-26 12:25:07 +0200

    5654

--- servidor/files/hydrosyn/endpoints/security.py
+++ servidor/files/hydrosyn/endpoints/security.py
@@ -10,7 +10,7 @@
 #notificacioens
 # extender epriodos
 
-
+#ber posy y añadir
 
 @router.get("/see-blacklist", response_class=HTMLResponse)
 async def see_blacklist_get(request: Request):
@@ -32,43 +32,3 @@ async def see_blacklist_get(request: Request):
 
 
 
-@router.get("/db-autoincrement")
-def autoincrement_get():
-  
-
-
-
-@router.post("/db-autoincrement")
-def autoincrement_post():
-    
-
-    
-    (3, 'es', 'Ver nombres lista negra', 'Permiso para ver los nombres en lista negra'),
-(3, 'en', 'View blacklist names', 'Permission to view blacklist names'),
-
--- permiso 4
-(4, 'es', 'Añadir nombres lista negra', 'Permiso para añadir nombres a la lista negra'),
-(4, 'en', 'Add blacklist names', 'Permission to add names to blacklist'),
-
--- permiso 5
-(5, 'es', 'Eliminar nombres lista negra', 'Permiso para eliminar nombres de la lista negra'),
-(5, 'en', 'Remove blacklist names', 'Permission to remove names from blacklist'),
-
-
-
-
--- permiso 6
-(6, 'es', 'Ver política de nombres', 'Permiso para ver la política de nombres'),
-(6, 'en', 'View naming policy', 'Permission to view naming policy'),
-
--- permiso 7
-(7, 'es', 'Cambiar política de nombres', 'Permiso para cambiar la política de nombres'),
-(7, 'en', 'Change naming policy', 'Permission to change naming policy'),
-
--- permiso 8
-(8, 'es', 'Ver política de contraseñas', 'Permiso para ver la política de contraseñas'),
-(8, 'en', 'View password policy', 'Permission to view password policy'),
-
--- permiso 9
-(9, 'es', 'Cambiar política de contraseñas', 'Permiso para cambiar la política de contraseñas'),
-(9, 'en', 'Change password policy', 'Permission to change password policy'),
--- servidor/files/hydrosyn/endpoints/settings.py
+++ servidor/files/hydrosyn/endpoints/settings.py
@@ -6,33 +6,28 @@ from logger import logger
 
 router = APIRouter(tags=["Web Settings"])
 
-@router.post("/change-lang-theme")
+@router.post("/change-language-theme")
 async def change_lang_theme_post(
-    request: Request,
-    lang: str = Form(...),
-    theme: str = Form(...),
-    next: str = Form(None)
+     request: Request,
+    type: str,  # "idioma" o "tema"
+    value: str  # "es", "en", "light", "dark"
 ):
 
     cookie_value = request.cookies.get("hydrosyn_session_id")
     if not cookie_value:
-        raise HTTPException(
-            status_code=401,  # or 400 if you prefer
-            detail="Session cookie not found"
+          return JSONResponse(
+            {"error": "NO cookie found"}, 
+            status_code=400
         )
 
-    request.state.lang = lang
-    request.state.theme = theme
+    #request.state.lang = lang
+    #request.state.theme = theme
     
     user_id=get_user_id_from_db(request.state.session_id)
     if user_id:
         # Actualizar preferencias del usuario en la base de datos
         await update_user_preferences_in_db(user_id, lang, theme)
     
-    if next=="/web/login":
-        logger.info("llegue al redirect login de settings")
-        return RedirectResponse(url="/web/login", status_code=303)
-    elif next=="/web/recover-password":
-        return RedirectResponse(url="/web/recover-password", status_code=303)
+
 
     # se comprueba si existe sesion cookie , se cambia en la base de datos, se comprueba el next y se envia al siguiente endpoint
