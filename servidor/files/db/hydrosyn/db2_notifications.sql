51026f936ff3286afd8275d8da04251f503b6719
startsys42 <startsys@proton.me>
2025-07-26 20:06:26 +0200

    435465

--- servidor/files/db/hydrosyn/db2_notifications.sql
+++ servidor/files/db/hydrosyn/db2_notifications.sql
@@ -3,10 +3,8 @@ USE hydrosyn_db;
 
 CREATE TABLE IF NOT EXISTS notifications (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY
-   --- should_send_email BOOLEAN NOT NULL DEFAULT FALSE
+    should_send_email BOOLEAN NOT NULL DEFAULT FALSE
 
-
-       -- Control por notificación
 
   
 );
