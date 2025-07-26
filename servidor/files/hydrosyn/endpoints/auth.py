8d8d6162867db4369a4e4f0e04f0c466a472c3ba
startsys42 <startsys@proton.me>
2025-07-26 22:27:11 +0200

    rert

--- servidor/files/hydrosyn/endpoints/auth.py
+++ servidor/files/hydrosyn/endpoints/auth.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter, Request
-from fastapi.templating import Jinja2Templates
+
 from fastapi.responses import HTMLResponse
 from endpoints.utils import get_user_preferences, ERROR_MESSAGES
 from fastapi.responses import PlainTextResponse
 from fastapi import Form, status
 from fastapi.responses import RedirectResponse
-from common.templates import templates
 from logger import logger
 from security.csrf import generate_csrf_token, validate_and_remove_csrf_token
 import os
