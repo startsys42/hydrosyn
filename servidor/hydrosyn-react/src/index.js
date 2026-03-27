a69feea5bb1f19f0d5fc7cddf704b52377d27f87
startsys42 <startsys@proton.me>
2026-03-27 23:33:35 +0100

    ff

--- servidor/hydrosyn-react/src/index.js
+++ servidor/hydrosyn-react/src/index.js
@@ -37,9 +37,12 @@ function AppWithMuiTheme() {
     };
     return (
         <MuiThemeProvider theme={muiTheme}>
+            <LocalizationProvider
 
-            <App />
-
+                localeText={datePickerLocaleText}
+            >
+                <App />
+            </LocalizationProvider>
         </MuiThemeProvider>
     );
 }
